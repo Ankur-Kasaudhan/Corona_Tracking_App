768176b9f2111288ae51e337c1be6ae1bfd5d06d
Ankur Kasaudhan <69796327+Ankur-Kasaudhan@users.noreply.github.com>
2021-09-18 14:40:48 +0530

    Add files via upload

---
+++ src/main/java/com/example/coronatracker/CountryActivity.java
@@ -0,0 +1,96 @@
+package com.example.coronatracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.coronatracker.api.ApiUtilities;
+import com.example.coronatracker.api.CountryData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class CountryActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private List<CountryData> list;
+    private ProgressDialog dialog;
+    private EditText search_bar;
+    private CountryAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country);
+
+        recyclerView = findViewById(R.id.countries);
+        list = new ArrayList<>();
+        search_bar = findViewById(R.id.search_bar);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        adapter = new CountryAdapter(this , list);
+        recyclerView.setAdapter(adapter);
+
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("Loading...");
+        dialog.setCancelable(false);
+        dialog.show();
+
+        ApiUtilities.getApiInterface().getCountryData().enqueue(new Callback<List<CountryData>>() {
+            @Override
+            public void onResponse(Call<List<CountryData>> call, Response<List<CountryData>> response) {
+                list.addAll(response.body());
+
+                adapter.notifyDataSetChanged();
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onFailure(Call<List<CountryData>> call, Throwable t) {
+                dialog.dismiss();
+
+                Toast.makeText(CountryActivity.this, ""+t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        search_bar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                filter(editable.toString());
+            }
+        });
+    }
+    private void filter(String text)
+    {
+        List<CountryData> filterlist = new ArrayList<>();
+        for (CountryData items : list)
+        {
+            if (items.getCountry().toLowerCase().contains(text.toLowerCase()))
+            {
+                filterlist.add(items);
+            }
+        }
+        adapter.filterList(filterlist);
+    }
+}
